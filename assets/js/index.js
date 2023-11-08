ec7472addbc8d6cd4316a5422eb2bf8847124e5d
yiching <doo8409255@gmail.com>
2023-11-08 15:37:29 +0800

    首頁

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,58 +1,88 @@
 $(document).ready(function () {
-    const imageInput = document.getElementById('imageInput');
-    const imagePreview = document.getElementById('imagePreview');
-    let cropper;
+    $("#birthday").datepicker({
+        dateFormat: "yy-mm-dd",
+        changeMonth: true,
+        changeYear: true
+    });
 
-    imageInput.addEventListener('change', function () {
-        const file = this.files[0];
-        const reader = new FileReader();
+    let cropperInstances = {};
 
-        reader.onload = function (e) {
-            imagePreview.src = e.target.result;
+    const $cropperModal = $('#cropperModal');
+    const $cropImageBtn = $('#cropImage');
 
-            if (cropper) {
-                cropper.destroy();
-            }
+    $('#UploadBackImage').on('change', function () {
+        openCropper(this, "BackImagePreview", "imageToCrop");
+    });
 
-            // 弹出模态框
-            $('#imageCropModal').modal('show');
+    $('#UploadFrontImage').on('change', function () {
+        openCropper(this, "FrontImagePreview", "imageToCrop");
+    });
 
-            cropper = new Cropper(imagePreview, {
-                aspectRatio: 1.6,  // 裁剪框宽高比例，可根据需求调整
-                responsive: true
+    $('#Close').on('click', function () {
+        $cropperModal.css('display', 'none');
+    });
+
+    // 绑定查詢按钮的点击事件
+    $('.searchBtnWrap button').on('click', function (e) {
+        e.preventDefault();
+
+        // 清空內容
+        $('.container').html('');
+
+        // 創建表格
+        const table = $('<table class="result-table"></table>');
+
+        const headers = ["Header1", "Header2", "Header3"]; // 替換Headers
+        const headerRow = $('<tr></tr>');
+        headers.forEach(headerText => {
+            const th = $('<th></th>').text(headerText);
+            headerRow.append(th);
+        });
+        table.append(headerRow);
+
+        // TODO: API
+
+        const newRow = $('<tr></tr>');
+        const cell1 = $('<td></td>').text("Data1");
+        const cell2 = $('<td></td>').text("Data2");
+        const cell3 = $('<td></td>').text("Data3");
+        newRow.append(cell1, cell2, cell3);
+        table.append(newRow);
+
+        $('.container').append(table);
+    });
+
+    function openCropper(inputElement, previewId, imageContainerId) {
+        const file = inputElement.files[0];
+        if (file) {
+            $cropperModal.css('display', 'flex');
+
+            const $image = $('<img>');
+            $image.attr('src', URL.createObjectURL(file));
+
+            const $preview = $('#' + previewId);
+            $preview.text('');
+
+            // 添加到 Cropper.js modal 中
+            const $imageToCrop = $('#' + imageContainerId);
+            $imageToCrop.empty();
+            $imageToCrop.append($image);
+
+            // 初始化 Cropper.js
+            cropperInstances[previewId] = new Cropper($image[0], {
+                viewMode: 1
             });
-        };
 
-        reader.readAsDataURL(file);
-    });
+            // 點擊裁切
+            $('#cropImage').on('click', function () {
+                const croppedImage = cropperInstances[previewId].getCroppedCanvas().toDataURL();
+                $('#' + previewId).css('background-image', 'url(' + croppedImage + ')');
 
-    $('#cropButton').on('click', function () {
-        const canvas = cropper.getCroppedCanvas();
-        if (canvas) {
-            canvas.toBlob(function (blob) {
-                // 在这里可以上传blob数据到服务器或进行其他操作
-                // 例如，使用FormData将blob数据上传到服务器
-                const formData = new FormData();
-                formData.append('croppedImage', blob, 'cropped_image.jpg');
+                inputElement.value = '';
+                $cropperModal.css('display', 'none');
 
-                // 示例：上传到服务器
-                // $.ajax({
-                //     url: 'upload.php', // 替换成你的上传处理URL
-                //     method: 'POST',
-                //     data: formData,
-                //     processData: false,
-                //     contentType: false,
-                //     success: function (response) {
-                //         // 处理上传成功的回调
-                //     },
-                //     error: function (error) {
-                //         // 处理上传失败的回调
-                //     }
-                // });
-            }, 'image/jpeg');
-
-            // 关闭模态框
-            $('#imageCropModal').modal('hide');
+                // cropperInstances[previewId].destroy();
+            });
         }
-    });
+    }
 });
