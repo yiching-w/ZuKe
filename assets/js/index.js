8c4ea05f030cade07fa72a7bf70f4211ac401415
yiching <doo8409255@gmail.com>
2023-11-14 12:02:44 +0800

    表格

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,55 +37,7 @@ $(document).ready(function () {
 
         // 清空內容
         $('#UploadInfo').hide();
-        // 顯示 #Table
-        $('.tableArea').empty();
         $('#Table').show();
-
-        // 創建容器 div
-        const containerDiv = $('<div class="result-container"></div>');
-
-        const headers = ["Header1", "Header2", "Header3"]; // 替換Headers
-
-        // 創建表頭區域
-        const headerDiv = $('<div class="header-row"></div>');
-        headers.forEach(headerText => {
-            const headerCell = $('<div class="header-cell"></div>').text(headerText);
-            headerDiv.append(headerCell);
-        });
-
-        // 將表頭區域添加到容器 div
-        containerDiv.append(headerDiv);
-
-        // TODO: 處理 API 數據，用迴圈添加數據行
-        const apiData = [
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"],
-            ["Data1", "Data2", "Data3"]
-            // 添加更多數據行...
-        ];
-
-        apiData.forEach(dataRow => {
-            const dataRowDiv = $('<div class="data-row"></div>');
-
-            dataRow.forEach(cellText => {
-                const dataCell = $('<div class="data-cell"></div').text(cellText);
-                dataRowDiv.append(dataCell);
-            });
-
-            // 將數據行添加到容器 div
-            containerDiv.append(dataRowDiv);
-        });
-
-        // 將容器 div 添加到容器元素
-        $('.tableArea').append(containerDiv);
     });
 
 
@@ -98,7 +50,6 @@ $(document).ready(function () {
             $image.attr('src', URL.createObjectURL(file));
 
             const $preview = $('#' + previewId);
-            $preview.text('');
 
             // 添加到 Cropper.js modal 中
             const $imageToCrop = $('#' + imageContainerId);
@@ -117,9 +68,28 @@ $(document).ready(function () {
 
                 inputElement.value = '';
                 $cropperModal.css('display', 'none');
+                $preview.find('b').hide();
 
                 // cropperInstances[previewId].destroy();
             });
+        }
+    }
+
+
+    UpdateInputState();
+    $('input[name="identityType"]').change(function () {
+        UpdateInputState();
+    });
+
+    function UpdateInputState() {
+        if ($('#inputIdentity').prop('checked')) {
+            $('#identity, #name, #birthday').prop('disabled', false);
+            $('#UploadFrontImage, #UploadBackImage').prop('disabled', true);
+            $('#FrontImagePreview, #BackImagePreview').addClass('disabled');
+        } else {
+            $('#identity, #name, #birthday').prop('disabled', true);
+            $('#UploadFrontImage, #UploadBackImage').prop('disabled', false);
+            $('#FrontImagePreview, #BackImagePreview').removeClass('disabled');
         }
     }
 });
---
+++ assets/js/records.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    GetData();
+
+    function GetData() {
+        // 創建容器 div
+        const containerDiv = $('<div class="result-container"></div>');
+
+        const headers = ["名字", "身分證", "生日", "評估結果"]; // 替換Headers
+
+        // 創建表頭區域
+        const headerDiv = $('<div class="header-row"></div>');
+        headers.forEach(headerText => {
+            const headerCell = $('<div class="header-cell"></div>').text(headerText);
+            headerDiv.append(headerCell);
+        });
+
+        // 將表頭區域添加到容器 div
+        containerDiv.append(headerDiv);
+
+        // TODO: 處理 API 數據，用迴圈添加數據行
+        const apiData = [
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"],
+            ["Data1", "Data2", "Data3", "Data4"]
+            // 添加更多數據行...
+        ];
+
+        apiData.forEach(dataRow => {
+            const dataRowDiv = $('<div class="data-row"></div>');
+
+            dataRow.forEach(cellText => {
+                const dataCell = $('<div class="data-cell"></div').text(cellText);
+                dataRowDiv.append(dataCell);
+            });
+
+            // 將數據行添加到容器 div
+            containerDiv.append(dataRowDiv);
+        });
+
+        // 將容器 div 添加到容器元素
+        $('.tableArea').append(containerDiv);
+    }
+})
